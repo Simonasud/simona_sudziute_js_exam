100bdaf3cddef7ca933214aa3ea6ea65591e6b50
Simona Sud <ssimona.sud@gmail.com>
2023-12-09 16:09:14 +0200

    trecia uzduotis

--- Task 3/script.js
+++ Task 3/script.js
@@ -1,3 +1,5 @@
+"use strict";
+console.log("script.js file was loaded");
 /* ------------------------------ TASK 3 -----------------------------------
 Parašykite JS kodą, kuris leis vartotojui paspaudus ant mygtuko "Show users"
 pamatyti vartotojus iš Github API (endpoint'as pateiktas žemiau).
@@ -11,4 +13,52 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const usersUrl = "https://api.github.com/users";
+
+const els = {
+  btnEl: document.getElementById("btn"),
+  outputEl: document.getElementById("output"),
+  messageEl: document.getElementById("message"),
+};
+// console.log("btnEl ===", els.btnEl);
+// console.log("outputEl ===", els.outputEl);
+// console.log("messageEl ===", els.messageEl);
+
+els.btnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  getUsers();
+});
+
+function getUsers() {
+  const fetchResult = fetch(usersUrl);
+  fetchResult
+    .then((resp) => resp.json())
+    .then((usersArr) => {
+      // console.log("usersArr ===", usersArr);
+      generateUserCards(usersArr);
+      els.messageEl.textContent = "";
+    })
+    .catch((error) => {
+      console.warn("ivyko klaida:", error);
+    });
+}
+
+function generateUserCards(arr) {
+  els.outputEl.innerHTML = "";
+  arr.forEach((usrObj) => {
+    const ulEl = document.createElement("ul");
+    ulEl.classList.add("userCard");
+
+    const liEl = document.createElement("li");
+    liEl.textContent = usrObj.login;
+
+    const avatarEl = document.createElement("img");
+    avatarEl.src = usrObj.avatar_url;
+    avatarEl.alt = `Avatar for ${usrObj.login}`;
+
+    ulEl.appendChild(liEl);
+    ulEl.appendChild(avatarEl);
+
+    els.outputEl.appendChild(ulEl);
+  });
+}
