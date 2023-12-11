6added1cf00e7054ba98abcb6c0f788eb00ed3a3
Simona Sud <ssimona.sud@gmail.com>
2023-12-11 09:40:15 +0200

    iskaidyta ir pridetas btn2

--- Task 4/script.js
+++ Task 4/script.js
@@ -28,31 +28,79 @@ els.btnEl.addEventListener("click", async () => {
   }
 });
 
-function generateCars(carData) {
-  const ulEl = document.createElement("ul");
-  ulEl.classList.add("car-list");
-
-  carData.forEach((car) => {
+function generateCars(data) {
+  data.forEach((car) => {
     const liEl = createCarListItem(car);
-    ulEl.append(liEl);
+    els.outputEl.append(liEl);
   });
-
-  els.outputEl.append(ulEl);
 }
 
 function createCarListItem(car) {
   const liEl = document.createElement("li");
   liEl.classList.add("car-card");
 
-  const brandEl = document.createElement("h2");
-  brandEl.textContent = car.brand;
+  const brandEl = createBrandElement(car.brand);
   liEl.append(brandEl);
 
-  car.models.forEach((model) => {
-    const modelPEl = document.createElement("p");
-    modelPEl.textContent = model;
-    liEl.append(modelPEl);
-  });
+  const divEl = createCarModelsContainer();
+  liEl.append(divEl);
+
+  const modelH3El = createModelHeadingElement();
+  divEl.append(modelH3El);
+
+  createInitialModels(car.models, divEl);
+
+  const btn2El = createLoadMoreButton(divEl, car.models);
+  liEl.append(btn2El);
 
   return liEl;
+}
+
+function createBrandElement(brand) {
+  const brandEl = document.createElement("h2");
+  brandEl.textContent = `Brand: ${brand}`;
+  return brandEl;
+}
+
+function createCarModelsContainer() {
+  const divEl = document.createElement("div");
+  divEl.classList.add("carModels");
+  return divEl;
+}
+
+function createModelHeadingElement() {
+  const modelH3El = document.createElement("h3");
+  modelH3El.textContent = "Car models: ";
+  return modelH3El;
+}
+
+function createInitialModels(models, container) {
+  models.slice(0, 5).forEach((model) => {
+    const modelPEl = createModelParagraphElement(model);
+    container.append(modelPEl);
+  });
+}
+
+function createModelParagraphElement(model) {
+  const modelPEl = document.createElement("p");
+  modelPEl.textContent = model;
+  return modelPEl;
+}
+
+function createLoadMoreButton(container, models) {
+  const btn2El = document.createElement("button");
+  btn2El.textContent = "Load more";
+  btn2El.classList.add("load-more-button");
+  btn2El.addEventListener("click", (e) => {
+    e.preventDefault();
+    container.innerHTML = "";
+
+    models.forEach((model) => {
+      const modelPEl = createModelParagraphElement(model);
+      container.append(modelPEl);
+    });
+    btn2El.style.display = "none";
+  });
+
+  return btn2El;
 }
